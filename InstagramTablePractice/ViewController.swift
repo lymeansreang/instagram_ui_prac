ddbc0e4a8175ae204723ad162c48c81a41e054c1
lymeansreng <lmsreang3@gmail.com>
2024-08-23 13:19:24 +0700

    Initial Commit

---
+++ InstagramTablePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramTablePractice
+//
+//  Created by Rithiya on 23/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
